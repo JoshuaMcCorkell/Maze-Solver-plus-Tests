2fa2a8a547a8c6800f172aeaf087e194c112f8d5
Joshua McCorkell <joshua@themccorkells.com>
2022-02-16 15:12:54 +1000

    16.02.2022 Stuff I learned

--- VideoTesting1.java
+++ VideoTesting1.java
@@ -1,46 +1,48 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class VideoTesting1{
 
-    static String x = "This is a Property!";
-    static int y = 100;
-
-    public static int square(int num) {
-        return num * num;
-    }
-
-    public static void main(String[] args) {
+    public static void gradesInOut() {
         Scanner consoleIn = new Scanner(System.in);
+        System.out.print("How many grades do you want to input: ");
+        int size = consoleIn.nextInt();
+        int[] grades = new int[size];
 
-        System.out.print("What is your name: ");
-        String name = consoleIn.nextLine();
-
-        if (name.equals("Joshua")) {
-            System.out.println("Hello Joshua!");
-        }
-        else {
-            System.out.println("You are NOT welcome, " + name + "!"); 
-            consoleIn.close(); 
-            return;
+        for (int i = 0; i < size; i++) {
+            System.out.print("#" + String.valueOf(i + 1) + ": ");
+            grades[i] = consoleIn.nextInt();
         }
 
-        int i = 0;
-        while (i < 5) {
-            System.out.println("i = " + i);
-            i++;
+        for (int i = 0; i < size; i++) {
+            System.out.print(grades[i] + "  ");
         }
 
-        for (i = 0; i < 5; i++) {
-            System.out.println("i = " + i);
-        }
-
-        i = 0;
-        do {
-            System.out.println("i = " + i);
-            i++;
-        } while(i < 5);
-        
         consoleIn.close();
     }
+
+    public static void readFileData() throws FileNotFoundException {
+        Scanner readFile = new Scanner(new File("Students.txt"));
+        
+        List<String> students = new ArrayList<>();
+        
+        while (readFile.hasNextLine()) {
+            students.add(readFile.nextLine());
+        }
+
+        for (String studentName : students){
+            System.out.println("Name: " + studentName);
+        }
+
+        readFile.close();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        //gradesInOut();
+        readFileData();
+    }
 }
-//37:30  (1:26:53)
+//1:(1:26:53)
